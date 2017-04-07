f6a51e671fef81ec8ceecbd3689ece833a4218ed
Corey Gibson <cgibsonmm@gmail.com>
2017-04-06 21:01:17 -0400

    added validation to Post title

--- spec/features/creating_posts_spec.rb
+++ spec/features/creating_posts_spec.rb
@@ -19,4 +19,13 @@ feature 'Creating Posts' do
     click_button 'Create Post'
     expect(page).to have_content('Error creating post')
   end
+
+  scenario 'post body should have greater than 20 chars' do
+    visit '/'
+    click_link 'New Post'
+    fill_in 'Title', with: 'This post should have a body'
+    fill_in 'Body', with: '123456789'
+    click_button 'Create Post'
+    expect(page).to have_content('Error creating post')
+  end
 end
